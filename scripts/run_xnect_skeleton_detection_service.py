6ce2ac7f28b371b2876c25874286bf7e4b9b202a
Stuart Golodetz <sgolodetz@gxstudios.net>
2021-10-18 13:10:51 +0100

    Get people masking based on Mask R-CNN working.

--- scripts/run_xnect_skeleton_detection_service.py
+++ scripts/run_xnect_skeleton_detection_service.py
@@ -8,7 +8,7 @@ import pygame
 
 from argparse import ArgumentParser
 from timeit import default_timer as timer
-from typing import Callable, List, Tuple
+from typing import Callable, List, Optional, Tuple
 
 from smg.comms.skeletons import SkeletonDetectionService
 from smg.pyxnect import SkeletonDetector
@@ -16,7 +16,7 @@ from smg.skeletons import Skeleton3D
 
 
 def make_frame_processor(skeleton_detector: SkeletonDetector, *, debug: bool = False, use_xnect_poses: bool) \
-        -> Callable[[np.ndarray, np.ndarray, np.ndarray], List[Skeleton3D]]:
+        -> Callable[[np.ndarray, np.ndarray, np.ndarray], Tuple[List[Skeleton3D], Optional[np.ndarray]]]:
     """
     Make a frame processor for a skeleton detection service that forwards to an XNect skeleton detector.
 
@@ -27,14 +27,14 @@ def make_frame_processor(skeleton_detector: SkeletonDetector, *, debug: bool = F
     """
     # noinspection PyUnusedLocal
     def detect_skeletons(colour_image: np.ndarray, depth_image: np.ndarray,
-                         world_from_camera: np.ndarray) -> List[Skeleton3D]:
+                         world_from_camera: np.ndarray) -> Tuple[List[Skeleton3D], Optional[np.ndarray]]:
         """
         Detect 3D skeletons in an RGB image using XNect.
 
         :param colour_image:        The RGB image.
         :param depth_image:         Passed in by the skeleton detection service, but ignored.
         :param world_from_camera:   The camera pose.
-        :return:                    The detected 3D skeletons.
+        :return:                    The detected 3D skeletons (the people mask will be rendered internally).
         """
         if debug:
             start = timer()
@@ -56,7 +56,7 @@ def make_frame_processor(skeleton_detector: SkeletonDetector, *, debug: bool = F
             # noinspection PyUnboundLocalVariable
             print("Detection Time: {}s".format(end - start))
 
-        return skeletons
+        return skeletons, None
 
     return detect_skeletons
 
